4d4a966f28441ff7e542be71383be510102e779f
henrik <pedersen>
2013-07-05 11:48:44 +0200

    forgot to save file

--- iReport/Rlocation.java
+++ iReport/Rlocation.java
@@ -6,19 +6,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Rlocation {
 
-	public static String getxyz(JavaPlugin i, String p){
-		org.bukkit.Location loc;
-		String s;
-		try {
-			loc = i.getServer().getPlayer(p).getLocation();
-			s = String.valueOf("x " + loc.getBlockX() + " y " + loc.getBlockY() + " z " + loc.getBlockZ());
-			return s;
-		} catch (Exception e) {
-			i.getLogger().log(Level.SEVERE, p+"is not online");
-		}
-		
-		
-		return null;
-	
-	}
+    public static String getxyz(JavaPlugin i, String p) {
+        org.bukkit.Location loc;
+        String s;
+        try {
+            loc = i.getServer().getPlayer(p).getLocation();
+            s = String.valueOf("x " + loc.getBlockX() + " y " + loc.getBlockY()
+                    + " z " + loc.getBlockZ());
+            return s;
+        } catch (Exception e) {
+            i.getLogger().log(Level.SEVERE, p + " is not online");
+        }
+
+        return null;
+
+    }
 }
