1472bc96ca14a7c804b8e0c2212ef1e42cd7cc0a
heni123321 <henrik@haue.dk>
2014-05-11 12:25:59 +0200

    remove unneeded import

--- src/main/java/iReport/iReport.java
+++ src/main/java/iReport/iReport.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ForkJoinPool;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
