d5bcfdde992a164dd1262eb1372da678062fbd8c
tudse145 <tudse145@gmail.com>
2014-08-17 14:43:58 +0200

    Made it easier to read reports while using /reports and /reports gui

--- src/main/java/iReport/commands/Reports.java
+++ src/main/java/iReport/commands/Reports.java
@@ -3,8 +3,8 @@ package iReport.commands;
 import static iReport.util.Data.init;
 import iReport.util.Data;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
@@ -30,12 +30,13 @@ public class Reports implements CommandExecutor {
         Inventory inv = calculate(Data.init().playermapo.size()); 
         if (sender instanceof HumanEntity &&  args.length == 1 && args[0].equalsIgnoreCase("gui")) { 
             for (UUID uuid : map2.keySet()) { 
+            	List<String> list = new ArrayList<String>();
                 ItemStack i	 = new ItemStack(Material.SKULL_ITEM, 1); 
                 i.setDurability((short) 3); 
                 SkullMeta meta = (SkullMeta) i.getItemMeta(); 
                 meta.setOwner(map1.get(uuid)); 
                 meta.setDisplayName(map1.get(uuid)); 
-                meta.setLore(Arrays.asList("UUID: " + uuid, "currentname: " + map1.get(uuid), map3.get(uuid), "username: " + map2.get(uuid))); 
+                meta.setLore(setLore(list, uuid));
                 i.setItemMeta(meta); 
                 inv.addItem(i); 
             } 
@@ -46,29 +47,49 @@ public class Reports implements CommandExecutor {
             try {
                 if (args[0].equalsIgnoreCase("uuid")) {
                     UUID u = UUID.fromString(args[1]);
-                    sender.sendMessage("UUID: " + u + " currentname: " + map1.get(u) + " " + map3.get(u) + "username: " + map2.get(u));
-                }
+                    for (String string : setLore(new ArrayList<String>(), u)) {
+    					sender.sendMessage(string);
+    				}                }
                 if (args[0].equalsIgnoreCase("usernameo")) {
                     UUID u = init().playermapor.get(args[1]);
-                    sender.sendMessage("UUID: " + u + " currentname: " + map1.get(u) + " " + map3.get(u) + "username: " + map2.get(u));
-                }
+                    for (String string : setLore(new ArrayList<String>(), u)) {
+    					sender.sendMessage(string);
+    				}                }
                 return true;
             } catch (Exception e) {
                 sender.sendMessage(ChatColor.RED + "invalid UUID");
             }
         } else {
-            Iterator<Entry<UUID, String>> iterator3 = map3.entrySet().iterator();
-            while (iterator3.hasNext()) {
-                Entry<UUID, String> e = iterator3.next();
-                UUID u = e.getKey();
-                sender.sendMessage("UUID: " + u + " currentname: " + map1.get(u) + " " + e.getValue() + "username: " + map2.get(u));
-            }
+        	if (map3.entrySet().size() == 0) {
+				sender.sendMessage(ChatColor.RED + "There is no reports");
+				return true;
+			}
+        	for (Entry<UUID, String> entry : map3.entrySet()) {
+        		UUID u = entry.getKey();
+                for (String string : setLore(new ArrayList<String>(), u)) {
+					sender.sendMessage(string);
+				}
+                sender.sendMessage(" ");
+			}
             return true;
         }
         return false;
     }
 
-    private Inventory calculate(int size) { 
+    private List<String> setLore(List<String> list, UUID uuid) {
+        Map<UUID, String> map1 = init().playermap;
+        Map<UUID, String> map2 = init().playermapo;
+        Map<UUID, String> map3 = init().playermapr;
+    	list.add("UUID: " + uuid);
+    	list.add("currentname: " + map1.get(uuid));
+    	for (String string : map3.get(uuid).split(";")) {
+			list.add(string);
+		}
+    	list.add("username: " + map2.get(uuid));
+		return list;
+	}
+
+	private Inventory calculate(int size) { 
         double f = size; 
         f = f/9; 
         if (f == size/9) { 
